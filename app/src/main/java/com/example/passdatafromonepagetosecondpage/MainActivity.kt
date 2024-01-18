939235455d294929da1117fd9d579303d19b63b7
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-18 14:56:04 +0530

    this is completed

--- app/src/main/java/com/example/passdatafromonepagetosecondpage/MainActivity.kt
+++ app/src/main/java/com/example/passdatafromonepagetosecondpage/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.passdatafromonepagetosecondpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val KEY  = "com.example.passdatafromonepagetosecondpage.MainActivity.KEY"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    val nextPageButton = findViewById<Button>(R.id.NextPageButton)
+    val nameText = findViewById<EditText>(R.id.enterYourName)
+    nextPageButton.setOnClickListener {
+        val intent = Intent(this , MainActivity2::class.java)
+        val yourName = nameText.text.toString()
+        intent.putExtra(KEY , yourName)
+        startActivity(intent)
+    }
+
     }
 }
--- app/src/main/java/com/example/passdatafromonepagetosecondpage/MainActivity2.kt
+++ app/src/main/java/com/example/passdatafromonepagetosecondpage/MainActivity2.kt
@@ -2,10 +2,14 @@ package com.example.passdatafromonepagetosecondpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val textFromFirstPage = findViewById<TextView>(R.id.secondPageText)
+        val getingNameFromFirstPage = intent.getStringExtra(MainActivity.KEY)
+        textFromFirstPage.text = getingNameFromFirstPage
     }
 }
